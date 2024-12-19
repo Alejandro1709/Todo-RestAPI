aa73a36bc9d6d77001545e6e6b41a36e569a328d
Alejandro LR <alejandro.lr.developer@hotmail.com>
2024-12-18 20:55:21 -0500

    Add: basic express server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,11 @@
+import express from "express";
 
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(2025, () => {
+  console.log("Server is up and running on port 2025");
+});
